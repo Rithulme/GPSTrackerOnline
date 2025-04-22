9b9af999a6ea26b25c961b06d486e3fb61396662
DESKTOP-NBR580J\Thomas Bogaerts <tho.bogaerts@gmail.com>
2025-04-22 21:07:17 +0200

    Adding websocket via sim test

---
+++ Tests/TestAutoSend/ModemSetup.ino
@@ -0,0 +1,235 @@
+void modemPowerOn() {
+  digitalWrite(PWR_PIN, HIGH);
+  delay(1000);  //Datasheet Ton min = 1S
+  digitalWrite(PWR_PIN, LOW);
+  Serial.println("SIM7000 switched on!");
+}
+
+void modemPowerOff() {
+  digitalWrite(PWR_PIN, HIGH);
+  delay(1500);  //Datasheet Toff min = 1.2S
+  digitalWrite(PWR_PIN, LOW);
+  Serial.println("SIM7000 switched off!");
+}
+
+bool forceModem()
+{
+  for (int j = 0; j < 20; j++) {
+    Serial2.print("AT\r\n");
+    String input = Serial2.readString();
+    if (input.indexOf("OK") >= 0) {
+      Serial.println("Modem responded");
+      return true;
+    }
+  }
+    Serial.println("Modem is not online!!!");
+    return false;
+}
+
+String readModem(){
+  String msg="";
+  bool lf = false;
+  while (Serial2.available()) {
+    byte byteUart = Serial2.read();
+    //printByteAsAscii(byteUart); //use this to visually print carriage-return and linefeed characters
+    
+    msg += (char)byteUart; //typecast byte to char
+    if(byteUart == '\n'){      
+      lf = true;
+    }
+  }
+  if(lf){
+    return msg;
+  }
+}
+
+bool sendModemAndProcessResponse(String ATcomm, String returnMsg){
+  Serial.print("Sending AT command:");Serial.println(ATcomm);
+  int retries=0;
+  String receivedMsg = "";
+  bool returnFound = false;
+  Serial2.println(ATcomm);
+  delay(1000);
+  while(retries < 10 && receivedMsg.indexOf(returnMsg) == -1){
+    receivedMsg = readModem();
+    retries++;
+    Serial.print("Try to receive expected return message:");Serial.print(returnMsg);Serial.print(" Attempt:");Serial.println(retries);
+    delay(1000);
+  }
+  if(retries > 10) {
+    Serial.print(returnMsg);Serial.print(" NOT found in return after sending command: ");Serial.print(ATcomm);Serial.println(" Sorry .... timed out / failed \n....RESET program, hang on for 10s");
+    //reset the module so we won't get stuck in for example the send SMS terminal mode
+    mainState = 0;
+    return false;
+  } else {
+    Serial.print(returnMsg);Serial.print(" found in return after sending command: ");Serial.print(ATcomm),Serial.println(" succesful, all done!");
+    return true;
+  }
+}
+
+bool sendModemAndProcessIP(String ATcomm){
+  Serial.print("Sending AT command:");Serial.println(ATcomm);
+  int retries=0;
+  String receivedMsg = "";
+  Serial2.println(ATcomm);
+  delay(1000);
+  while(retries < 10 && countOccurence(receivedMsg , '.') < 4){
+    receivedMsg = readModem();
+    retries++;
+    Serial.print("Try to receive expected return message:");Serial.print("Attempt:");Serial.println(retries);
+    delay(1000);
+  }
+  if(retries > 10) {
+    Serial.print("Ip NOT found in return after sending command: ");Serial.print(ATcomm);Serial.println(" Sorry .... timed out / failed \n....RESET program, hang on for 10s");
+    //reset the module so we won't get stuck in for example the send SMS terminal mode
+    mainState = 0;
+    return false;
+  } else {
+    Serial.print("IP found in return after sending command: ");Serial.print(ATcomm),Serial.println(" succesful, all done!");
+    return true;
+  }
+}
+
+void ManageModemState(){
+  static int previousState = -1;
+  static unsigned long previousStep;
+  bool commandOk = false;
+  String message;
+  switch(mainState){
+    case 0:
+      if (previousState != mainState) {
+        previousState = mainState;
+        previousStep = millis();
+      }
+
+      if(millis() - previousState > 7500){
+        mainState = 1;
+      }
+
+      break;
+
+    case 1:
+      if (previousState != mainState) {
+        pinMode(PWR_PIN, OUTPUT);
+        digitalWrite(PWR_PIN, LOW);
+        modemPowerOff();
+        previousStep = millis();
+        previousState = mainState;
+      }
+
+      if(millis() - previousState > 7500){        
+        mainState = 2;
+      }
+
+      break;
+
+    case 2:
+      if (previousState != mainState) {
+        modemPowerOn();
+        previousStep = millis();
+        previousState = mainState;
+      }
+
+      if(millis() - previousState > 7500){        
+        mainState = 3;
+      }
+
+      break;
+
+      case 3:
+        previousState = mainState;
+        if(forceModem()){
+          mainState = 4;
+        }
+        else {
+          Serial.println("Reset modem");
+        }
+
+        break;
+      
+      case 4:
+        previousState = mainState;
+        message = readModem();
+
+        if(message.indexOf("PSUTTZ") != -1){
+        mainState = 5;
+        }
+
+        break;
+      
+      case 5:
+        //try sending command once, if it fails 'commandOk' is always false, after 1 second previous state is reset to trigger command again
+        if (previousState != mainState) {
+          previousState = mainState;
+          previousStep = millis();
+          commandOk = sendModemAndProcessResponse("AT+CIPSHUT","SHUT OK");
+          Serial.println("at state 5");
+        }       
+        
+        if(commandOk){
+          mainState = 6;
+        }
+        else{
+          if (millis() - previousStep > 1000) {
+            previousState = 4;
+          }
+        }   
+
+      case 6:
+        //try sending command once, if it fails 'commandOk' is always false, after 1 second previous state is reset to trigger command again
+        if (previousState != mainState) {
+          previousState = mainState;
+          previousStep = millis();
+          commandOk = sendModemAndProcessResponse("AT+CSTT=\"internet.proximus.be\"","OK");
+          Serial.println("at state 6");
+        }       
+        
+        if(commandOk){
+          mainState = 7;
+        }
+        else{
+          if (millis() - previousStep > 1000) {
+            previousState = 5;
+          }
+        }
+
+      case 7:
+        //try sending command once, if it fails 'commandOk' is always false, after 1 second previous state is reset to trigger command again
+        if (previousState != mainState) {
+          previousState = mainState;
+          previousStep = millis();
+          commandOk = sendModemAndProcessResponse("AT+CIICR","OK");
+          Serial.println("at state 7");
+        }       
+        
+        if(commandOk){
+          mainState = 8;
+        }
+        else{
+          if (millis() - previousStep > 1000) {
+            previousState = 6;
+          }
+        }
+
+      case 8:
+        //try sending command once, if it fails 'commandOk' is always false, after 1 second previous state is reset to trigger command again
+        if (previousState != mainState) {
+          previousState = mainState;
+          previousStep = millis();
+          commandOk = sendModemAndProcessIP("AT+CIFSR");
+          Serial.println("at state 8");
+        }       
+        
+        if(commandOk){
+          mainState = 9;
+        }
+        else{
+          if (millis() - previousStep > 1000) {
+            previousState = 7;
+          }
+        }
+
+      case 9:
+        digitalWrite(startupLEDPin, true);
+  }
+}
---
+++ Tests/TestAutoSend/TestAutoSend.ino
@@ -0,0 +1,21 @@
+//modem data
+const int PIN_DTR = 25;
+const int PIN_TX = 27;
+const int PIN_RX = 26;
+const int PWR_PIN = 4;
+
+const int startupLEDPin = 22;
+
+int mainState = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, 26, 27);
+
+  pinMode(startupLEDPin, OUTPUT);
+}
+
+void loop() {
+  ManageModemState();
+
+}
